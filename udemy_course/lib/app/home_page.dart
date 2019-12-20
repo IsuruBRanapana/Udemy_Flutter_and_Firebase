ba13ed2479981b35afa5b541db859e1edc4b54d1
IsuruBRanapana <raaisurub.ranapana@gmail.com>
2019-12-20 22:04:59 +0530

    Updated

--- udemy_course/lib/app/home_page.dart
+++ udemy_course/lib/app/home_page.dart
@@ -1,6 +1,19 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
+
+  HomePage({@required this.onSignOut});
+  final VoidCallback onSignOut;
+  Future<void> _signOut() async {
+    try {
+      await FirebaseAuth.instance.signOut();
+      onSignOut();
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -10,7 +23,7 @@ class HomePage extends StatelessWidget {
         ),
         actions: <Widget>[
           FlatButton(
-              onPressed: () {},
+              onPressed:_signOut,
               child: Text(
                 'Logout',
                 style: TextStyle(color: Colors.white, fontSize: 18.0),
