2cea5cf772c402907df191dc6ac497f8da4b056f
IsuruBRanapana <raaisurub.ranapana@gmail.com>
2019-12-20 22:39:43 +0530

    Updated

---
+++ udemy_course/lib/services/auth.dart
@@ -0,0 +1,17 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class Auth{
+
+  final _firebaseAuth = FirebaseAuth.instance;
+  Future<FirebaseUser> currentUser() async{
+    return _firebaseAuth.currentUser();
+  }
+
+  Future<FirebaseUser> signInAnonymously() async{
+    final authResult = await _firebaseAuth.signInAnonymously();
+    return authResult.user;
+  }
+  Future<void> signOut() async{
+    await _firebaseAuth.signOut();
+  }
+}
