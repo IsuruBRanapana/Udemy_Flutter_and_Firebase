9c22ce11646474565ab204abdccff36fc5643a3b
IsuruBRanapana <raaisurub.ranapana@gmail.com>
2019-12-23 09:12:23 +0530

    Updated

--- udemy_course/lib/services/auth.dart
+++ udemy_course/lib/services/auth.dart
@@ -70,6 +70,8 @@ class Auth implements AuthBase {
 
   @override
   Future<void> signOut() async {
+    final googleSignIn=GoogleSignIn();
+    await googleSignIn.signOut();
     await _firebaseAuth.signOut();
   }
 }
