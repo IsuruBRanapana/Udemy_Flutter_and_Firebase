bb18086c515174249cc3f09e070ef74023e2d9de
IsuruBRanapana <raaisurub.ranapana@gmail.com>
2019-12-27 11:52:28 +0530

    Updated

--- udemy_course/lib/app/sign_in/sign_in.dart
+++ udemy_course/lib/app/sign_in/sign_in.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:udemy_course/app/sign_in/email_sign_in_page.dart';
-import 'package:udemy_course/app/sign_in/sign_in_bloc.dart';
+import 'package:udemy_course/app/sign_in/sign_in_manager.dart';
 import 'package:udemy_course/app/sign_in/sign_in_button.dart';
 import 'package:udemy_course/app/sign_in/social_sign_in_button.dart';
 import 'package:udemy_course/common_widgets/platform_exception_alert_dialog.dart';
@@ -9,16 +9,22 @@ import 'package:udemy_course/services/auth.dart';
 import 'package:flutter/services.dart';
 
 class SignInPage extends StatelessWidget {
-  const SignInPage({Key key, @required this.bloc}) : super(key: key);
-  final SignInBloc bloc;
+  const SignInPage({Key key, @required this.manager,@required this.isLoading}) : super(key: key);
+  final SignInManager manager;
+  final bool isLoading;
   static Widget create(BuildContext context) {
     final auth = Provider.of<AuthBase>(context);
-    return Provider<SignInBloc>(
-      create: (_) => SignInBloc(auth: auth),
-      dispose: (context, bloc) => bloc.dispose(),
-      child: Consumer<SignInBloc>(
-        builder: (context, bloc, _) => SignInPage(
-          bloc: bloc,
+    return ChangeNotifierProvider<ValueNotifier<bool>>(
+      create: (_) =>ValueNotifier<bool>(false),
+      child: Consumer<ValueNotifier<bool>>(
+        builder: (_,isLoading,__)=> Provider<SignInManager>(
+          create: (_) => SignInManager(auth: auth, isLoading: isLoading),
+          child: Consumer<SignInManager>(
+            builder: (context, manager, _) => SignInPage(
+              manager: manager,
+              isLoading: isLoading.value,
+            ),
+          ),
         ),
       ),
     );
@@ -26,7 +32,7 @@ class SignInPage extends StatelessWidget {
 
   Future<void> _signInAnonymously(BuildContext context) async {
     try {
-      await bloc.signInAnonymously();
+      await manager.signInAnonymously();
     } on PlatformException catch (e) {
       _showSignInErrors(context, e);
     }
@@ -34,7 +40,7 @@ class SignInPage extends StatelessWidget {
 
   Future<void> _signInWithGoogle(BuildContext context) async {
     try {
-      await bloc.signInWithGoogle();
+      await manager.signInWithGoogle();
     } on PlatformException catch (e) {
       if (e.code != 'ERROR_ABORTED_BY_USER') {
         _showSignInErrors(context, e);
@@ -44,7 +50,7 @@ class SignInPage extends StatelessWidget {
 
   Future<void> _signInWithFacebook(BuildContext context) async {
     try {
-      await bloc.signInWithFacebook();
+      await manager.signInWithFacebook();
     } on PlatformException catch (e) {
       if (e.code != 'ERROR_ABORTED_BY_USER') {
         _showSignInErrors(context, e);
@@ -75,17 +81,12 @@ class SignInPage extends StatelessWidget {
         title: Text('Time Tracker'),
         elevation: 2.0,
       ),
-      body: StreamBuilder<bool>(
-          stream: bloc.isLoadingStream,
-          initialData: false,
-          builder: (context, snapshot) {
-            return _buildContent(context, snapshot.data);
-          }),
+      body:  _buildContent(context),
       backgroundColor: Colors.grey[200],
     );
   }
 
-  Widget _buildContent(BuildContext context, bool isLoading) {
+  Widget _buildContent(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16.0),
       child: Column(
@@ -94,7 +95,7 @@ class SignInPage extends StatelessWidget {
         children: <Widget>[
           SizedBox(
             height: 50.0,
-            child: _headerWidgetBuilder(isLoading),
+            child: _headerWidgetBuilder(),
           ),
           SizedBox(
             height: 48.0,
@@ -151,7 +152,7 @@ class SignInPage extends StatelessWidget {
     );
   }
 
-  Widget _headerWidgetBuilder(bool isLoading) {
+  Widget _headerWidgetBuilder() {
     if (isLoading) {
       return Center(
         child: CircularProgressIndicator(),
