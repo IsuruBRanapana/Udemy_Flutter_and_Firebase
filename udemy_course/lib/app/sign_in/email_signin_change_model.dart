9048975da1bf065a712316283a08f32de58a5137
IsuruBRanapana <raaisurub.ranapana@gmail.com>
2020-01-02 22:07:32 +0530

    Updated

--- udemy_course/lib/app/sign_in/email_signin_change_model.dart
+++ udemy_course/lib/app/sign_in/email_signin_change_model.dart
@@ -1,20 +1,38 @@
 import 'package:flutter/foundation.dart';
 import 'package:udemy_course/app/sign_in/email_signin_model.dart';
 import 'package:udemy_course/app/sign_in/validator.dart';
+import 'package:udemy_course/services/auth.dart';
 
 class EmailSignInChangeModel with EmailAndPasswordValidators, ChangeNotifier {
   EmailSignInChangeModel({
+    @required this.auth,
     this.email: '',
     this.password: '',
     this.formType: EmailSignInFormType.signIn,
     this.isLoading: false,
     this.submitted: false,
   });
+
+  final AuthBase auth;
   String email;
   String password;
   EmailSignInFormType formType;
   bool isLoading;
   bool submitted;
+
+  Future<void> submit() async {
+    updateWith(isLoading: true,submitted: true);
+    try {
+      if (formType == EmailSignInFormType.signIn) {
+        await auth.signInWithEmailAndPassword(email, password);
+      } else {
+        await auth.createUserWithEmailAndPassword(email, password);
+      }
+    } catch (e) {
+      updateWith(isLoading: false);
+      rethrow;
+    }
+  }
 
   String get primaryButtonText {
     return formType == EmailSignInFormType.signIn
@@ -58,4 +76,21 @@ class EmailSignInChangeModel with EmailAndPasswordValidators, ChangeNotifier {
     this.submitted = submitted ?? this.submitted;
     notifyListeners();
   }
+  void toggleFormType(){
+    final formType=this.formType == EmailSignInFormType.signIn
+        ? EmailSignInFormType.Register
+        : EmailSignInFormType.signIn;
+    updateWith(
+      email: '',
+      password: '',
+      submitted: false,
+      formType: formType,
+      isLoading: false,
+    );
+  }
+
+  void updatePassword(String password) => updateWith(password: password);
+
+  void updateEmail(String email) => updateWith(email: email);
+
 }
