535c417fa27165561f0c748128e52184288a53a7
IsuruBRanapana <raaisurub.ranapana@gmail.com>
2020-01-16 20:02:41 +0530

    Updated

--- udemy_course/lib/app/home/jobs_page.dart
+++ udemy_course/lib/app/home/jobs_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:udemy_course/app/home/models/job.dart';
 import 'package:udemy_course/common_widgets/platform_alert_dialog.dart';
 import 'package:udemy_course/services/auth.dart';
 import 'package:udemy_course/services/database.dart';
@@ -25,14 +26,15 @@ class JobsPage extends StatelessWidget {
       _signOut(context);
     }
   }
-  Future <void> _createJob(BuildContext context) async{
-    final database=Provider.of<Database>(context);
-    await database.createJob({
-      'name':'Blogging',
-      'rate_per_hour':10,
-    });
 
+  Future<void> _createJob(BuildContext context) async {
+    final database = Provider.of<Database>(context);
+    await database.createJob(Job(
+      name: 'Painting',
+      ratePerHour: 12,
+    ));
   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -52,10 +54,8 @@ class JobsPage extends StatelessWidget {
       ),
       floatingActionButton: FloatingActionButton(
         child: Icon(Icons.add),
-        onPressed: ()=>_createJob(context),
+        onPressed: () => _createJob(context),
       ),
     );
   }
-
-
 }
--- udemy_course/lib/services/database.dart
+++ udemy_course/lib/services/database.dart
@@ -1,17 +1,18 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/foundation.dart';
+import 'package:udemy_course/app/home/models/job.dart';
 
 abstract class Database{
-  Future <void> createJob(Map<String, dynamic> jobData);
+  Future <void> createJob(Job job);
 }
 
 class FirestoreDatabase extends Database{
   FirestoreDatabase({@required this.uid}): assert(uid != null);
   final String uid;
 
-  Future <void> createJob(Map<String, dynamic> jobData) async{
+  Future <void> createJob(Job job) async{
     final path='/users/$uid/jobs/job_abc';
     final documentReference=Firestore.instance.document(path);
-    await documentReference.setData(jobData);
+    await documentReference.setData(job.toMap());
   }
 }
